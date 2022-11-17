38f7a906e8a7f4a8cb6727a736389e87a10d98a2
Anders Flotten <anders3835@hotmail.com>
2022-11-17 03:12:33 +0000

    Implement load tweets function via ajax get request. Add validation edge cases to check for empty/too long cases

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,71 +5,60 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-$(document).ready(function() {
+$(document).ready(function () {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      console.log($tweet)
+      $('#tweetBox').append($tweet);
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  for (const tweet of tweets) {
-    // calls createTweetElement for each tweet
-    const $tweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    console.log($tweet)
-    $('#tweetBox').append($tweet);
+    }
 
   }
-  
-}
 
-const createTweetElement = function(tweet) {
-  let $tweet = (`
+  const createTweetElement = function (tweet) {
+    let $tweet = (`
   <article class="tweets">
   <header class="tweetHeader">
     <div class="person">
-      <img class="avatar" src="https://i.imgur.com/73hZDYK.png"></img>
-      <h5 class="username">Newton</h5>
+      <img class="avatar" src="${tweet.user.avatars}"></img>
+      <h5 class="username"> ${tweet.user.name} </h5>
     </div>
-    <div class="handle"> @SirIssac </div>
+    <div class="handle"> ${tweet.user.handle} </div>
   </header>
-  <div class="tweetContent">Hello World!</div>
+  <div class="tweetContent"> ${tweet.content.text} </div>
   <footer class="tweetFooter">
-    <span class="timeCreated">Time Created</span>
+    <span class="timeCreated"> ${timeago.format(tweet.created_at)}</span>
     <div class="icons">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -80,16 +69,47 @@ const createTweetElement = function(tweet) {
   </footer>
 </article>`);
 
-return $tweet;
-}
+    return $tweet;
+  }
 
-renderTweets(data);
 
-//const $tweet = createTweetElement(tweetData);
 
-//$('#tweetBox').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  $("#tweetForm").submit(function (event) {
 
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
+    event.preventDefault();
+    const max = 140;
+    const total = $(this).find("#tweet-text").val().length;
+
+    if (!total) {
+      alert("Tweet cannot be empty!")
+    } else if (total - max > 0) {
+      alert("Tweet is too long!")
+    } else {
+      let str = $("#tweetForm").serialize();
+
+      $.ajax({
+        url: "/tweets ", method: "POST",
+        data: str,
+        success: function (response) { console.log(response) },
+        error: function (error) { console.log(error) }
+      });
+    };
+  });
+
+
+  const loadTweets = function () {
+    $.ajax({
+      type: "GET",
+      dataType: "JSON",
+      url: "/tweets"
+
+    })
+      .then(function (newTweet) {
+        console.log("THIS HERE", newTweet)
+        renderTweets(newTweet);
+      })
+  };
+
+  loadTweets();
 
 });
